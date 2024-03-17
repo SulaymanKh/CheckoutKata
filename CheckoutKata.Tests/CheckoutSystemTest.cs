c3f6acf3ee9e6e8872864114e146d80d488cff8a
SulaymanKh <137774114+SulaymanKh@users.noreply.github.com>
2024-03-17 15:13:01 +0000

    Added Scan Item Test

--- CheckoutKata.Tests/CheckoutSystemTest.cs
+++ CheckoutKata.Tests/CheckoutSystemTest.cs
@@ -2,9 +2,53 @@ namespace CheckoutKata.Tests;
 
 public class CheckoutSystemTest
 {
-    [Fact]
-    public void Test1()
-    {
+    private Dictionary<string, (int, int)> pricingRules;
 
+    public CheckoutSystemTest()
+    {
+        pricingRules = new Dictionary<string, (int, int)>
+        {
+            {"A", (1, 50) },
+            {"B", (1, 30) },
+            {"C", (1, 20) },
+            {"D", (1, 15) }
+        };
+    }
+
+    [Fact]
+    public void Scan_ExistingItem_AddsItemToList()
+    {
+        //Arrange
+        var checkout = new Checkout(pricingRules);
+        string sku = "A";
+
+        //Act
+        checkout.Scan(sku);
+
+        //Assert
+        Assert.Contains(sku, checkout._scannedItems);
+    }
+}
+
+interface ICheckout
+{
+    void Scan(string item);
+}
+
+public class Checkout : ICheckout
+{
+    private readonly Dictionary<string, (int, int)> _pricingRules;
+
+    public readonly List<string> _scannedItems = new List<string>();
+
+    public Checkout(Dictionary<string, (int, int)> pricingRules)
+    {
+        _pricingRules = pricingRules;
+        _scannedItems = new List<string>();
+    }
+
+    public void Scan(string sku)
+    {
+        _scannedItems.Add(sku);
     }
 }
